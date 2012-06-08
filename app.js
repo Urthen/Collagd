5c317be24253ef7a37e538155c6b92dbbc3d2141
Michael Pratt <urthen@gmail.com>
2012-06-08 01:04:46 -0500

    bugfix

--- app.js
+++ app.js
@@ -7,29 +7,14 @@ var express = require("express"),
 
 db_module.connect(config);
 
-// Bootstrap models
-var models_path = __dirname + "/app/models";
-var model_files = fs.readdirSync(models_path);
-
-model_files.forEach(function (file) {
-	if (file.substr(-3) === ".js") {
-		require(models_path + "/" + file);
-	}
-});
 
 // Setup Express Application
 var app = express.createServer();
 require("./app/settings").boot(app, config);
 
-// Bootstrap controllers
-var controllers_path = __dirname + "/app/controllers",
-	controller_files = fs.readdirSync(controllers_path);
-
-controller_files.forEach(function (file) {
-	if (file.substr(-3) === ".js") {
-		require(controllers_path + "/" + file)(app);
-	}
-});
+// Setup controllers
+require('./app/controllers/img')(app);
+require('./app/controllers/index')(app);
 
 // Good to go, brah
 var port = process.env.PORT || 5000;
