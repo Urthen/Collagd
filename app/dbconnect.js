2574b275f515673096f82aba6b66d20ffef3c982
Michael Pratt <urthen@gmail.com>
2012-06-15 04:45:44 -0500

    fixing dbs?

--- app/dbconnect.js
+++ app/dbconnect.js
@@ -4,10 +4,15 @@ var mongoose = require("mongoose"),
 module.exports.db = null;
 module.exports.connect = function (config) {
 	var server_config = new mongodb.Server(config.db.hostname, config.db.port, {auto_reconnect: true, native_parser: true}),
-		user = config.db.user ? config.db.user + '@' : '',
+		user = config.db.username ? config.db.username + ":" + config.db.password + "@" : '',
 		uri = "mongodb://" + user + config.db.hostname + ":" + config.db.port + "/" + config.db.db;
 	
 	module.exports.db = new mongodb.Db(config.db.db, server_config, {});
+
+	if (config.db.username) {
+		module.exports.db.authenticate(config.db.username, config.db.password)	
+	}
+	
 
 	mongoose.connect(uri);
 
